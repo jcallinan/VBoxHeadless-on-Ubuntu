13e4e3c5677445095ddf02e7b4985a3eea001f2e
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-18 19:38:06 -0400

    Root access on Ubuntu

---
+++ root-on-ubuntu.sh
@@ -0,0 +1,15 @@
+# Note that this is not recommended until and unless you are an expert and aware of what you are typing or doing:
+$ sudo -i
+
+# OR
+$ sudo -s
+
+# Above command will start /bin/bash as a root shell so that you can enter a root user command without using sudo command.
+
+# How do I login as root user?
+# Open the terminal application and simply type the following sudo command
+$ sudo bash
+
+# OR
+$ sudo -s
+# from https://www.cyberciti.biz/faq/ubuntu-linux-root-password-default-password/
