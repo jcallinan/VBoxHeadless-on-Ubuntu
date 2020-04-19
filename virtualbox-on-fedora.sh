b771ed8a1b8ac43f2ea97a6470a90615f9d12565
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-18 22:10:33 -0400

    Fedora version

---
+++ virtualbox-on-fedora.sh
@@ -0,0 +1,25 @@
+# from: https://www.zealfortechnology.com/2012/06/install-oracle-virtualbox-on-fedora.html
+
+#First, you need to download the repository file from:
+#http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
+
+
+#Then you need to move the file "virtualbox.repo" to the folder /etc/yum.repos.d/. In the terminal, navigate to the location where you kept the file and use the command:
+sudo mv virtualbox.repo /etc/yum.repos.d/
+
+
+#Then you need to install dkms as follows:
+sudo yum install dkms
+
+
+#To install the latest version of VirtualBox use the command:
+sudo yum install VirtualBox-4.1
+#Note: The package name was change from virtualbox-4.1 to VirtualBox-4.1
+
+
+#If you prefer the older version of VirtualBox just change the version number such as:
+sudo yum install virtualbox-3.2
+sudo yum install virtualbox-3.1
+sudo yum install virtualbox-3.0
+
+
